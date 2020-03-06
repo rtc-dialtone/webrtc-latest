3bd28830a01f93dc8eb55e75eb8bcc67d6e80e26
Ben Greenier <ben@bengreenier.com>
2020-03-06 20:01:03 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ uname | grep 'MSYS'
 curl -o depot_tools.zip https://storage.googleapis.com/chrome-infra/depot_tools.zip
 unzip depot_tools.zip -d depot_tools
 rm depot_tools.zip
+export PATH=$PATH:$PWD/depot_tools
 git clone https://webrtc.googlesource.com/src src
 cd src
-export PATH=$PATH:$PWD/depot_tools
 export DEPOT_TOOLS_WIN_TOOLCHAIN=0
 gclient sync
 gn gen out/x64/Debug --args="is_clang=false is_debug=true rtc_include_tests=false target_cpu=\"x64\""
