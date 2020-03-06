b7976fd337ff35f0667759cdfc3d6ff51a9f76c6
Ben Greenier <ben@bengreenier.com>
2020-03-06 20:30:09 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ export _PREV_PATH=$PATH
 export PATH=C:\Windows\system32:$(which git | sed s/git//):$PWD/depot_tools
 git clone https://webrtc.googlesource.com/src src
 cd src
-which python
 export DEPOT_TOOLS_WIN_TOOLCHAIN=0
 gclient sync
 gn gen out/x64/Debug --args="is_clang=false is_debug=true rtc_include_tests=false target_cpu=\"x64\""
